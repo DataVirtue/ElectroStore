6f943202bc519330e994b748664cc892460156e6
Akshay Pawar <akshay@Akshays-MacBook-Air.local>
2021-12-07 18:00:14 +0530

    start script and port changes

--- app.js
+++ app.js
@@ -140,8 +140,8 @@ app.get('/search', (req, res) => {
 
 
 
-
-app.listen(3000, () => {
-    console.log("Listening on port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
 
