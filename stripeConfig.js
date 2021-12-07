d32e57fa4a61df7d2cd85d7d4359bccf561a4b83
Akshay Pawar <akshay@Akshays-MacBook-Air.local>
2021-12-07 19:26:43 +0530

    updated stripe urls again

--- stripeConfig.js
+++ stripeConfig.js
@@ -6,5 +6,5 @@ const endpointSecret = `${process.env.STRIPE_ENDPOINT_SECRET}`;
 
 module.exports.stripe = stripe;
 module.exports.endpointSecret = endpointSecret;
-module.exports.cancelUrl = "https://evening-reef-33083.herokuapp.com//payments/cancel"
-module.exports.successUrl = "https://evening-reef-33083.herokuapp.com//payments/success"
+module.exports.cancelUrl = "https://evening-reef-33083.herokuapp.com/payments/cancel"
+module.exports.successUrl = "https://evening-reef-33083.herokuapp.com/payments/success"
